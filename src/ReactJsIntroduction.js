88f995b80af9d7f624d041d259260298eb0699d6
Bijay Kumar Yadav <bijay.yadav@cloudyfox.com>
2020-01-20 18:01:41 +0545

    Simple Introduction in ReactJsIntroduction.js

---
+++ src/ReactJsIntroduction.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const name = "Bijay Kumar Yadav";
+export const element = <h1>Hello, {name}</h1>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom'
+import { element } from './ReactJsIntroduction'
+
+ReactDom.render(
+    element,
+    document.getElementById('root')
+);
