1d24437910a9e04598fde9d5450050477eedbb38
Bijay Kumar Yadav <bijay.yadav@cloudyfox.com>
2020-01-21 17:05:28 +0545

    controlledComponent.js: Overall, this makes it so that <input type="text">, <textarea>, and <select> all work very similarly - they all accept a value attribute that you can use to implement a controlled component.

--- src/forms/controlledComponent.js
+++ src/forms/controlledComponent.js
@@ -5,34 +5,75 @@ class NameForm extends React.Component {
         super(props);
 
         this.state = {
-            name: ''
+            name: '',
+            description: '',
+            gender: 'Male',
+            multiple: ['']
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleDescription = this.handleDescription.bind(this);
+        this.handleGender = this.handleGender.bind(this);
+        this.handleMutliple = this.handleMutliple.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({
-            name: event.target.value
+            name: event.target.value.toUpperCase()
+        });
+    }
+
+    handleDescription(event) {
+        this.setState({
+            description: event.target.value
+        });
+    }
+
+    handleGender(event) {
+        this.setState({
+            gender: event.target.value
+        });
+    }
+
+    handleMutliple(event) {
+        this.setState({
+            multiple: Array.from(event.target.selectedOptions, (item) => item.value)
         });
     }
 
     handleSubmit(event) {
-        alert('A name submitted: ' + this.state.name);
+        alert('Name: ' + this.state.name + '\nDescription: ' + this.state.description + `\nGender: ${this.state.gender}`);
         event.preventDefault();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name: <input type="text" value={this.state.name} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-                <br />
-                <label>
-                    Name: {this.state.name}
-                </label>
+                <div>
+                    <label>
+                        Name: <input type="text" tabIndex="1" value={this.state.name} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Description: <textarea tabIndex="2" placeholder="There is some text" value={this.state.description} onChange={this.handleDescription} />
+                    </label>
+                </div>
+                <div>
+                    <select value={this.state.gender} onChange={this.handleGender}>
+                        <option value="0">Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
+                <div>
+                    <select multiple={true} value={this.state.multiple} onChange={this.handleMutliple}>
+                        <option value="0">Select Multiple</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+                <input type="submit" tabIndex="3" value="Submit" />
             </form>
         );
     }
