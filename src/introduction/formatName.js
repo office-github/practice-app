cf164a423e537f9ed8c01ab46082c3831783342b
Bijay Kumar Yadav <bijay.yadav@cloudyfox.com>
2020-01-21 09:59:47 +0545

    Introduction: Added getGreeting function.

--- src/introduction/formatName.js
+++ src/introduction/formatName.js
@@ -4,13 +4,19 @@ function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 }
 
+function getGreeting(user) {
+    if (user) {
+        return <h1> Hello, {formatName(user)} </h1>;
+    }
+
+    return <h1>Hello, Stranger</h1>
+}
+
 const user = {
     firstName: 'Bijay',
     lastName: 'Yadav'
 }
 
 export const element = (
-    <h1>
-        Hello, {formatName(user)}
-    </h1>
+    getGreeting(user)
 );
