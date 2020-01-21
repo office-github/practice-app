2b4820d22dfe2014b061a5712170ff604c379c08
Bijay Kumar Yadav <bijay.yadav@cloudyfox.com>
2020-01-21 10:50:42 +0545

    Updating the Rendered Element: Created Closures and IIFE.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 // import { element } from './introduction/printName'
 // import { element } from './introduction/formatName'
-import { elementOne, elementTwo } from './introduction/identical'
+// import { elementOne, elementTwo } from './introduction/identical'
 
 
 /**
@@ -25,7 +26,33 @@ import { elementOne, elementTwo } from './introduction/identical'
  * Indentical Syntax For Creating React Elements
  */
 
-ReactDOM.render(
-    elementTwo,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     elementTwo,
+//     document.getElementById('root')
+// );
+
+/**
+ * Updating the Rendered Element: With our knowledge so far, the only way to update the UI is to create a new element, and pass it to ReactDOM.render().
+ */
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {seconds()} seconds.</h2>
+        </div>
+    );
+
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+let seconds = (
+    function () {
+        let count = 0;
+        return function () {
+            return count++;
+        }
+    }
+)();
+
+setInterval(tick, 1000);
